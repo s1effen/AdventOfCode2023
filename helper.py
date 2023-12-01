26f07a20eb44f3f40a62a32da67a258f96491097
Steffen <git@haesler.xyz>
2023-12-01 23:29:46 +0100

    organized main and helper

---
+++ helper.py
@@ -0,0 +1,9 @@
+from importlib import import_module
+from datetime import date
+
+def run(day=date.today().day, test=False):
+    print("\n######################################################")
+    print("#".ljust(24) + "Day {:02d}".format(day) + "#".rjust(24))
+    print("######################################################")
+    run = getattr(import_module("day{:02d}.day{:02d}".format(day, day)), "run")
+    run("{:02d}".format(day), test)
--- main.py
+++ main.py
@@ -1,9 +1,2 @@
-import workspace
 import helper
-from importlib import import_module
-
-def run(day, test=False):
-   run = getattr(import_module("day{:02d}.day{:02d}".format(day,day)), "run")
-   run("{:02d}".format(day),test)
-
-run(1,test=False)
+helper.run()
