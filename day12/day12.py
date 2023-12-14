28a54a72c8339e0e478e850f4d969a39b85ca5a2
Steffen Haesler <haesler@peasec.tu-darmstadt.de>
2023-12-14 19:50:58 +0100

    day12+13 half through

--- day12/day12.py
+++ day12/day12.py
@@ -1,11 +1,22 @@
 import sys
 
+def count(row: list, item):
+    return sum(1 for i in row if i == item)
+
+def solve(line):
+    print("{} with {} unknown springs and pattern to check {}".format(line[0], count(line[0], '?'), line[1]))
+
+
+
+
+
 def input(f):
     d = []
     with open(f, "r") as f:
         for index,line in enumerate(f):
             if line.strip():
-                d.append(line.strip())
+                d.append(line.strip().split(' '))
+                d[-1][1] = list(map(lambda x: int(x), d[-1][1].split(',')))
             else:
                 False
     return d
@@ -13,7 +24,7 @@ def input(f):
 def part1(data):
     print("---------------------------\nPart1:\n---------------------------")
     for d in data:
-        True
+        solve(d)
     print("Answer: {}".format(42))
 
 def part2(data):
--- day13/day13.py
+++ day13/day13.py
@@ -1,26 +1,79 @@
 import sys
+import numpy as np
 
 def input(f):
     d = []
+    temp = []
     with open(f, "r") as f:
         for index,line in enumerate(f):
-            if line.strip():
-                d.append(line.strip())
+            if line.strip() != "":
+                temp.append(list(line.strip()))
             else:
-                False
+                d.append(np.array(temp))
+                temp = []
+    d.append(np.array(temp))
     return d
+
+def smudge(field):
+    unchanged = checkmirror(field, 'h'), checkmirror(field, 'v')
+    for x in range(field.shape[1]):
+        for y in range(field.shape[0]):
+            fieldcopy = field.copy()
+            if field[y][x] == '#':
+                fieldcopy[y][x] = '.'
+            else:
+                fieldcopy[y][x] = '#'
+            changed = checkmirror(fieldcopy, 'h'), checkmirror(fieldcopy, 'v')
+            if changed[0] > 0 and changed[0] != unchanged[0]:
+                print("Old: {} New: {}".format(unchanged, changed))
+                print(max(changed))
+                return changed[0]*100
+            if changed[1] > 0 and changed[1] != unchanged[1]:
+                print("Old: {} New: {}".format(unchanged, changed))
+                print(max(changed))
+                return changed[1]
+    print("No new reflection found")
+    return 0
+
+def checkmirror(field,direction='v'):
+    if direction == 'v':
+        for i in range(1,field.shape[1]):
+            if i < field.shape[1]/2:
+                l = i
+            else:
+                l = field.shape[1] - i
+            #print("Left with mirror at {} position {} with size {}:\n {}".format(i,direction,l,field[:, i-l:i]))
+            #print("Right with mirror at {} position {} with size {}:\n {}".format(i,direction ,l,np.flip(field[:, i:i+l],1)))
+            if np.array_equal(field[:, i-l:i],np.flip(field[:, i:i+l],1)):
+                #print("Vertical reflection between {} and {}".format(i,i+1))
+                return i
+    if direction == 'h':
+        for i in range(1, field.shape[0]):
+            if i < field.shape[0] / 2:
+                l = i
+            else:
+                l = field.shape[0] - i
+            #print("Top with mirror at {} position {} with size {}:\n {}".format(i,direction,l,field[i-l:i,:]))
+            #print("Bottom with mirror at {} position {} with size {}:\n {}".format(i,direction ,l,np.flip(field[i:i+l,:],0)))
+            if np.array_equal(field[i - l:i, :], np.flip(field[i:i + l,:], 0)):
+                #print("Horizontal reflection between {} and {}".format(i, i + 1))
+                return i
+    return 0
 
 def part1(data):
     print("---------------------------\nPart1:\n---------------------------")
+    answer = 0
     for d in data:
-        True
-    print("Answer: {}".format(42))
+        answer += checkmirror(d,direction='v')
+        answer += checkmirror(d,direction='h')*100
+    print("Answer: {}".format(answer))
 
 def part2(data):
     print("\n---------------------------\nPart2:\n---------------------------")
+    answer = 0
     for d in data:
-        True
-    print("Answer: {}".format(42))
+       answer += smudge(d)
+    print("Answer: {}".format(answer))
 
 def run(day,test=False):
     if test:
